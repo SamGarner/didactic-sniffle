a3c8e0c4e3558f8b19137ba84e4b40916675071c
SamGarner <samuel.w.garner@gmail.com>
2020-10-15 10:56:42 -0500

    remove unused @matched_guess_indices and @matched_code_indices and clean up comments

--- mastermind_working_version.rb
+++ mastermind_working_version.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: false
 
-# Initialize Game (Game)
-# create game board (Game)
-# create two players (Game)
-# create six colors for guessing (Board)
-# generate 4 piece secret code (Player)
-# set guess counter = 12 (Game)  **
-
 # Game - initializes new game and game loop
 class Game
   def initialize
@@ -26,7 +19,7 @@ class Game
   end
 
   def play
-    while @guess_counter > 0 #@game_over.zero?  #noted   
+    while @guess_counter > 0
       @board.display_last_guess unless @guess_counter == 12
       @codebreaker.guess_secret_code
       puts 'Invalid guess format, please try again' unless
@@ -35,9 +28,8 @@ class Game
         @board.valid_guess?(@codebreaker.new_guess_array)
       @board.add_guess_to_board(@guess_counter, @codebreaker.new_guess_array)
       @board.reset_match_calculators
-      @board.reset_remaining_indices_to_check ###
+      @board.reset_remaining_indices_to_check
       @board.check_exact_matches
-      #@board.get_remaining_indices_to_check
       @board.check_color_only_remaining_matches
       @board.display_guess_results
       if @board.exact_match_count == 4
@@ -68,7 +60,7 @@ class Board
     guess_array.all? { |guess| @colors.include?(guess) } && guess_array.length == 4
   end
 
-  def add_guess_to_board(guess_counter, guess_array) # then can just compare board rows to each other below.
+  def add_guess_to_board(guess_counter, guess_array)
     @row_for_guess = 13 - guess_counter
     @board_array[@row_for_guess] = guess_array
   end
@@ -81,8 +73,6 @@ class Board
   def reset_match_calculators
     @exact_match_count = 0
     @color_only_match_count = 0
-    @matched_guess_indices = []
-    @matched_code_indices = []
   end
 
   def reset_remaining_indices_to_check
@@ -103,7 +93,7 @@ class Board
   #   @remaining_code_indices = (0..3).reject { |n| @matched_code_indices.include?(n) }
   # end
 
-  def check_color_only_remaining_matches#(guess_array) # try it with the indices and not colors
+  def check_color_only_remaining_matches
     @code_copy = @board_array[0].dup
     @guess_copy = @board_array[@row_for_guess].dup
     @remaining_indices.each do |n|
@@ -118,8 +108,8 @@ class Board
   end
 
   def display_guess_results
-    ## troubleshooting inspect, remove
-    puts @board_array[0].inspect
+    ## use line below for troubleshooting - displays the secret code
+    ## puts @board_array[0].inspect
     ##
     if @exact_match_count == 4
       puts "\nCongrats - you cracked the code!\n"
@@ -127,17 +117,9 @@ class Board
       puts "\n#{@exact_match_count} exact matches and #{@color_only_match_count} other color only matches"
     end
   end
-    # while @remaining_guess_indices.length > 0
-    #   if @remaining_guess_indices[0]
-    # # @guess_colors_to_check.each do |color|
-    # #   # loop inside here over each element could be cleaner?
-    # #   if @code_colors_to_check.include?(color)
-    # #     @color_only_match_count += 1
-    # #     color = ''
-    # #     @code_colors_to_check
 end
 
-# player - controls codebreaker guess input and codemaker reply 
+# player - controls codebreaker guess input and codemaker reply
 class Player
   attr_reader :new_guess_array
 
@@ -148,7 +130,7 @@ class Player
     else  
       # WILL NEED TO BE UPDATED IF PLAYER ROLES AREN'T BEING HARDCODED AND BECOME AN INPUT OPTION
       # THE ELSE IS NOT CURRENTLY HANDLED CORRECTLY AND GAMEPLAY WILL CONTINUE
-      puts "Please retry with valid player types(s): 'human' or 'computer'" # 'human' or 'computer'
+      puts "Please retry with valid player types(s): 'human' or 'computer'"
     end
   end
 
@@ -159,17 +141,13 @@ class Player
   def guess_secret_code
     puts 'choose four colors (green, blue, yellow, white, black, pink - duplicates OK)'\
     ' separated by spaces to guess the code:'
-    @new_guess_array = gets.chomp.downcase.split()#gsub(' ','').split(',')
+    @new_guess_array = gets.chomp.downcase.split()
   end
-
-  # tricky one since using all board things but needs to stay in player for when human player takes this role?
-  # def generate_secret_code #will need to be expanded when want to reverse computer/human roles
-  #    @board.board_array[0].map! { |n| n = colors.sample }
-  # end
 end
 
 game = Game.new
 game.play
+
 # Game loop
 # (show previous guess unless guess counter = 12) (Board)
 # guess 4 piece code (Player)
@@ -181,26 +159,3 @@ game.play
 # subtract one turn from guess counter (Game)
 # display number of turns remaining (Game)
   # end if 0 turns left (Game)
-
-# lezioni
-# when have nested arrays: Board[0] gives first array, Board[0][1] gives second element of first array
-# .include? returns true if the given object is present in the array
-# using .dup to copy an array before manipulating it, otherwise pointing at same space in memory and will mutate both
-# game_over.zero? preferred to game_over == 0 ?
-# multiline string example:
-#     puts 'choose four colors (green, blue, yellow, white, black, pink - duplicates OK)'\
-#     ' separated by spaces to guess the code:'
-# @board_array = Array.new(12) { Array.new(4) }  --> makes an array of 12 (sub)arrays of 4
-# reject example (removed with refactoring, see screenshot)
-      # def check_exact_matches
-      #   (0..3).each do |n|
-      #     if @board_array[@row_for_guess][n] == @board_array[0][n]
-      #       @exact_match_count += 1
-      #       @matched_guess_indices << n
-      #       @matched_code_indices << n
-      #     end
-      #   end
-      # end
-
-      # def get_remaining_indices_to_check
-      #   @remaining_guess_indices = (0..3).reject { |n| @matched_guess_indices.include?(n) }
